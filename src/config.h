2c7306e7f79626df765152d2a1f868ab2fbbedf3
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-01-11 14:44:03 +0100

    do all variables then

--- src/config.h
+++ src/config.h
@@ -1,11 +1,12 @@
-/* config.h */
 #pragma once
 
 #include <stdint.h>
 
-static const int32_t SQUARE_WIDTH        = 24;
-// static const int32_t SQUARE_BORDER_WIDTH = 3;
-// static const int32_t PUZZLE_HEIGHT       = 20;
-// static const int32_t PUZZLE_WIDTH        = 10;
-// static const int32_t SIDEBAR_WIDTH       = SQUARE_WIDTH * 6;
-// static const int32_t WAR_ZONE_WIDTH      = PUZZLE_WIDTH * SQUARE_WIDTH;
+static const int32_t SQUARE_WIDTH = 24;
+static const int32_t SQUARE_BORDER_WIDTH = 3;
+static const int32_t PUZZLE_HEIGHT = 20;
+static const int32_t PUZZLE_WIDTH = 10;
+static const int32_t SIDEBAR_WIDTH = SQUARE_WIDTH * 6;
+static const int32_t WAR_ZONE_WIDTH = PUZZLE_WIDTH * SQUARE_WIDTH;
+static const int32_t CANVAS_WIDTH = SIDEBAR_WIDTH + WAR_ZONE_WIDTH;
+static const int32_t CANVAS_HEIGHT = PUZZLE_HEIGHT + SQUARE_WIDTH;
