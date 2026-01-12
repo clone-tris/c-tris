b8ab09aef85171d5dd3e30bffef733d44edaae46
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-01-12 18:47:45 +0100

    decpare appstate

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_video.h"
 #include <stdio.h>
 #define SDL_MAIN_USE_CALLBACKS 1
 #include "config.h"
@@ -8,13 +10,15 @@
 #include <SDL3/SDL_main.h>
 #include <time.h>
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
+typedef struct AppState {
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+} AppState;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
-  (void)appstate;
   (void)argc;
   (void)argv;
+
   SDL_SetAppMetadata("C-tris", "1.0", "com.clone-tris.c-tris");
 
   if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -22,8 +26,16 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     return SDL_APP_FAILURE;
   }
 
+  AppState *as = (AppState *)SDL_calloc(1, sizeof(AppState));
+  if (!as) {
+    SDL_Log("Couldn't allocate space for AppState");
+    return SDL_APP_FAILURE;
+  }
+
+  *appstate = as;
+
   if (!SDL_CreateWindowAndRenderer(
-        "c-tris", CANVAS_WIDTH, CANVAS_HEIGHT, 0, &window, &renderer
+        "c-tris", CANVAS_WIDTH, CANVAS_HEIGHT, 0, &as->window, &as->renderer
       )) {
     SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
@@ -33,20 +45,21 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
-  (void)appstate;
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderClear(renderer);
+  AppState *as = (AppState *)appstate;
 
-  SDL_SetRenderDrawColor(renderer, 69, 69 * 2, 69 * 3, 255);
+  SDL_SetRenderDrawColor(as->renderer, 0, 0, 0, 255);
+  SDL_RenderClear(as->renderer);
+
+  SDL_SetRenderDrawColor(as->renderer, 69, 69 * 2, 69 * 3, 255);
   SDL_FRect rect = {
     (float)2 * (float)SQUARE_WIDTH,
     (float)2 * (float)SQUARE_WIDTH,
     (float)SQUARE_WIDTH,
     (float)SQUARE_WIDTH
   };
-  SDL_RenderFillRect(renderer, &rect);
+  SDL_RenderFillRect(as->renderer, &rect);
 
-  SDL_RenderPresent(renderer);
+  SDL_RenderPresent(as->renderer);
 
   return SDL_APP_CONTINUE;
 }
@@ -60,6 +73,9 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-  (void)appstate;
   (void)result;
+  AppState *as = (AppState *)appstate;
+  SDL_DestroyRenderer(as->renderer);
+  SDL_DestroyWindow(as->window);
+  SDL_free(as);
 }
