f4580f13e5cd347a4f0305f01f2de843f5c87d06
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2026-01-16 20:09:00 +0100

    typedef forward declaration of screen

--- src/engine/screen.h
+++ src/engine/screen.h
@@ -1,16 +1,16 @@
 #pragma once
 
 #include "SDL3/SDL_scancode.h"
-struct Screen;
+typedef struct Screen Screen;
 
 typedef struct ScreenVTable {
-  void (*draw)(struct Screen *);
-  void (*update)(struct Screen *);
-  void (*keyDown)(struct Screen *, SDL_Scancode);
-  void (*mouse_button_up)(struct Screen *);
-  void (*cleanup)(struct Screen *);
+  void (*draw)(Screen *);
+  void (*update)(Screen *);
+  void (*keyDown)(Screen *, SDL_Scancode);
+  void (*mouse_button_up)(Screen *);
+  void (*cleanup)(Screen *);
 } ScreenVTable;
 
-typedef struct Screen {
+struct Screen {
   const ScreenVTable *vtable;
-} Screen;
+};
