ee21d11ff2837e3da2a3e48ac798dbed257f18e6
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2026-01-16 18:01:45 +0100

    some colors

---
+++ src/colors.c
@@ -0,0 +1,9 @@
+#include "colors.h"
+
+void Color_SetRenderDrawColor(SDL_Renderer *renderer, Uint32 color) {
+  Uint8 r = (color >> 24) & 0xFF;
+  Uint8 g = (color >> 16) & 0xFF;
+  Uint8 b = (color >> 8) & 0xFF;
+  Uint8 a = color & 0xFF;
+  SDL_SetRenderDrawColor(renderer, r, g, b, a);
+}
