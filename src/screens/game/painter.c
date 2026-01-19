51fe9c1642c3b127149d003a0a60e7d29c83bc5b
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-01-19 20:14:36 +0100

    static means it's not realloated

--- src/screens/game/painter.c
+++ src/screens/game/painter.c
@@ -4,24 +4,24 @@
 #include "engine/painter.h"
 #include <SDL3/SDL_rect.h>
 
-static const SDL_FRect SIDEBAR_RECT = {
-  .x = 0, .y = 0, .w = (float)SIDEBAR_WIDTH, .h = (float)CANVAS_HEIGHT
-};
-
-static const SDL_Point PLAYFIELD_REF = {.x = SIDEBAR_WIDTH, .y = 0};
-static const SDL_FRect PLAYFIELD_RECT = {
-  .x = (float)PLAYFIELD_REF.x,
-  .y = (float)PLAYFIELD_REF.y,
-  .w = (float)WAR_ZONE_WIDTH,
-  .h = (float)CANVAS_HEIGHT
-};
-
 void drawPlayfield(Shape *player) {
-  drawGuide(&PLAYFIELD_RECT);
-  drawShape(player, &PLAYFIELD_REF);
+  static const SDL_Point origin = {.x = SIDEBAR_WIDTH, .y = 0};
+  static const SDL_FRect rect = {
+    .x = (float)origin.x,
+    .y = (float)origin.y,
+    .w = (float)WAR_ZONE_WIDTH,
+    .h = (float)CANVAS_HEIGHT
+  };
+
+  drawGuide(&rect);
+  drawShape(player, &origin);
 }
 
 void drawSidebar() {
+  static const SDL_FRect rect = {
+    .x = 0, .y = 0, .w = (float)SIDEBAR_WIDTH, .h = (float)CANVAS_HEIGHT
+  };
+
   App_SetRenderDrawColor(App_renderer, UI_SIDEBAR_BACKGROUND);
-  SDL_RenderFillRect(App_renderer, &SIDEBAR_RECT);
+  SDL_RenderFillRect(App_renderer, &rect);
 }
