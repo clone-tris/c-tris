51abc601c091dbd4fb930000b610e229bf6bed7f
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-01-20 17:58:29 +0100

    constant color helpers

--- src/colors.c
+++ src/colors.c
@@ -7,11 +7,11 @@ void App_SetRenderDrawColor(SDL_Renderer *renderer, uint32_t color) {
   SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
 }
 
-SDL_Color App_Color(uint32_t color) {
-  Uint8 r = (color >> 24) & 0xFF;
-  Uint8 g = (color >> 16) & 0xFF;
-  Uint8 b = (color >> 8) & 0xFF;
-  Uint8 a = color & 0xFF;
+SDL_Color App_Color(const uint32_t color) {
+  const Uint8 r = (color >> 24) & 0xFF;
+  const Uint8 g = (color >> 16) & 0xFF;
+  const Uint8 b = (color >> 8) & 0xFF;
+  const Uint8 a = color & 0xFF;
 
   return (SDL_Color){.r = r, .g = g, .b = b, .a = a};
 }
